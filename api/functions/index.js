5ba92ff884e5f36529f385973d7cde527ed5db09
Bruno Mariano <bsignx@gmail.com>
2020-08-11 10:50:18 -0300

    feat: user registration

--- api/functions/index.js
+++ api/functions/index.js
@@ -1,56 +1,113 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const firebase = require("firebase");
 
 const app = express();
 
 admin.initializeApp();
 
-app.get('/posts', (req, res) => {
- admin
-   .firestore()
-   .collection("posts")
-   .orderBy('createdAt', 'desc')
-   .get()
-   .then((data) => {
-     let posts = [];
-     data.forEach((doc) => {
-       posts.push({
-         postId: doc.id,
-         body: doc.data().body,
-         userHandle: doc.data().userHandle,
-         createdAt: doc.data().createdAt,
-       });
-     });
-     return res.json(posts);
-   })
-   .catch((err) => console.log(err));
-})
-
-app.post('/post', (req, res) => {
-if (req.method !== "POST") {
-  return res.status(400).json({ error: "Method not allowed" });
-}
-const newPost = {
-  body: req.body.body,
-  userHandle: req.body.userHandle,
-  createdAt: new Date().toISOString(),
+const config = {
+  apiKey: "AIzaSyCq1Ck4ImZ4ll4aotUexSl7DEQgFz8nB5k",
+  authDomain: "helpu-social-media.firebaseapp.com",
+  databaseURL: "https://helpu-social-media.firebaseio.com",
+  projectId: "helpu-social-media",
+  storageBucket: "helpu-social-media.appspot.com",
+  messagingSenderId: "453350706070",
+  appId: "1:453350706070:web:69816207335861a6a9abb7",
+  measurementId: "G-Q74JGZ9CN8",
 };
 
-admin
-  .firestore()
-  .collection("posts")
-  .add(newPost)
-  .then((doc) => {
-    return res.json({
-      messsage: `document ${doc.id} created successfully`,
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+app.get("/posts", (req, res) => {
+  db.collection("posts")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let posts = [];
+      data.forEach((doc) => {
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(posts);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.post("/post", (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Method not allowed" });
+  }
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+
+  db.collection("posts")
+    .add(newPost)
+    .then((doc) => {
+      return res.json({
+        messsage: `document ${doc.id} created successfully`,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: `something went wrong. ${err}` });
     });
-  })
-  .catch((err) => {
-    return res.status(500).json({ error: `something went wrong. ${err}` });
-  });
-})
+});
 
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
+  let userToken, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      userToken = token;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ userToken })
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
 
 exports.api = functions.https.onRequest(app);
