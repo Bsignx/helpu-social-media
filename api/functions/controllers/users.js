df9bdbdd482b0637265fbe0da4050813c44763ce
Bruno Mariano <bsignx@gmail.com>
2020-08-16 10:29:43 -0300

    feat: create and get notifications

--- api/functions/controllers/users.js
+++ api/functions/controllers/users.js
@@ -65,7 +65,9 @@ exports.signup = (req, res) => {
       if (err.code === "auth/email-already-in-use") {
         return res.status(400).json({ email: "Email is already in use" });
       } else {
-        return res.status(500).json({ error: err.code });
+        return res
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
       }
     });
 };
@@ -91,11 +93,11 @@ exports.login = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if (err.code === "auth/wrong-password") {
-        return res
-          .status(403)
-          .json({ general: "Wrong credentials, please try again" });
-      } else return res.status(500).json({ error: err.code });
+      // auth/wrong-password
+      // auth/user-not-user
+      return res.status(403).json({
+        general: "Wrong credentials, please try again",
+      });
     });
 };
 
@@ -135,7 +137,80 @@ exports.getAuthenticatedUser = (req, res) => {
       data.forEach((doc) => {
         userData.likes.push(doc.data());
       });
+      return db
+        .collection("notifications")
+        .where("recipient", "==", req.user.handle)
+        .orderBy("createdAt", "desc")
+        .limit(10)
+        .get();
+    })
+    .then((data) => {
+      userData.notifications = [];
+      data.forEach((doc) => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          screamId: doc.data().screamId,
+          type: doc.data().type,
+          read: doc.data().read,
+          notificationId: doc.id,
+        });
+      });
       return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+exports.getUserDetails = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      userData.user = doc.data();
+      return db
+        .collection("posts")
+        .where("userHandle", "==", req.params.handle)
+        .orderBy("createdAt", "desc")
+        .get();
+    })
+    .then((data) => {
+      userData.posts = [];
+      data.forEach((doc) => {
+        userData.posts.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          userImage: doc.data().userImage,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          postId: doc.data().postId,
+        });
+      });
+      return res.json({ userData });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+exports.markNotificationsRead = (req, res) => {
+  let batch = db.batch();
+  req.body.forEach((notificationId) => {
+    const notification = db.doc(`/notifications/${notificationId}`);
+    batch.update(notification, { read: true });
+  });
+  batch
+    .commit()
+    .then(() => {
+      return res.json({ message: "Notifications marked read" });
     })
     .catch((err) => {
       console.error(err);
--- api/functions/index.js
+++ api/functions/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable promise/always-return */
 const functions = require("firebase-functions");
 const express = require("express");
 
+const { db } = require("./util/admin");
 const { FBAuth } = require("./util/fbAuth");
 const {
   postOnePost,
@@ -17,6 +19,8 @@ const {
   uploadImage,
   addUserDetails,
   getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead,
 } = require("./controllers/users");
 
 const app = express();
@@ -28,7 +32,7 @@ app.get("/post/:postId", getPost);
 app.post("/post/:postId/comment", FBAuth, commentOnPost);
 app.get("/posts/:postId/like", FBAuth, likePost);
 app.get("/posts/:postId/unlike", FBAuth, unlikePost);
-app.delete('/posts/:postId', FBAuth, deletePost);
+app.delete("/posts/:postId", FBAuth, deletePost);
 
 // User routes
 app.post("/signup", signup);
@@ -36,5 +40,73 @@ app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region("southamerica-east1").https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region("southamerica-east1")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            postId: doc.id,
+          });
+        }
+        return;
+      })
+      .catch((err) => console.error(err));
+  });
+exports.deleteNotificationOnUnLike = functions
+  .region("southamerica-east1")
+  .firestore.document("likes/{id}")
+  .onDelete((snapshot) => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+exports.createNotificationOnComment = functions
+  .region("southamerica-east1")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            postId: doc.id,
+          });
+        }
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
